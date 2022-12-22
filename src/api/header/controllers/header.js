f0bc5ad54f3858caecde3f4111bc5e415c6ee95b
anaclara-rethink <102759771+anaclara-rethink@users.noreply.github.com>
2022-12-22 10:02:14 -0300

    Update home e header

--- src/api/header/controllers/header.js
+++ src/api/header/controllers/header.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- *  home controller
+ *  header controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::home.home');
+module.exports = createCoreController('api::header.header');
