5b576560b8c2e4d798b4a46aa26bd2bc50bade6c
anaclara-rethink <102759771+anaclara-rethink@users.noreply.github.com>
2022-12-28 10:17:18 -0300

    getHeader

--- src/api/header/controllers/header.js
+++ src/api/header/controllers/header.js
@@ -1,9 +1,36 @@
-'use strict';
+"use strict";
 
 /**
  *  header controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::header.header');
+const getHeader = (entity) => {
+  const menu = [];
+  for (const key in entity.Menu) {
+    menu.push(entity.Menu[key].TextOption);
+  }
+
+  const acordeon = [];
+  for (const key in entity.Acordeon) {
+    acordeon.push(entity.Acordeon[key].TextOption);
+  }
+
+  return Object.freeze({
+    logo: entity.logo.url,
+    menu,
+    titleAcordeon: entity.TitleAcordeon,
+    acordeon,
+  });
+};
+
+module.exports = createCoreController("api::header.header", ({ strapi }) => ({
+  async find(ctx) {
+    const { query } = ctx;
+    const entity = await strapi.entityService.findOne("api::header.header", 1, {
+      ...query,
+    });
+    return getHeader(entity);
+  },
+}));
--- src/api/home/controllers/home.js
+++ src/api/home/controllers/home.js
@@ -1,9 +1,40 @@
-'use strict';
+"use strict";
 
 /**
  *  home controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::home.home');
+const getHome = (entity) => {
+  return Object.freeze({
+    title: entity.Title,
+    subTitle: entity.Subtitle,
+    background: entity.Background.url,
+    leftCard: {
+      description: entity.screenAreas.LeftCard.Description,
+      background: entity.screenAreas.LeftCard.background.url,
+      icon: entity.screenAreas.LeftCard.icon.url,
+    },
+    middleCard: {
+      description: entity.screenAreas.MiddleCard.Description,
+      background: entity.screenAreas.MiddleCard.background.url,
+      icon: entity.screenAreas.MiddleCard.icon.url,
+    },
+    rightCard: {
+      description: entity.screenAreas.RightCard.Description,
+      background: entity.screenAreas.RightCard.background.url,
+      icon: entity.screenAreas.RightCard.icon.url,
+    },
+  });
+};
+
+module.exports = createCoreController("api::home.home", ({ strapi }) => ({
+  async find(ctx) {
+    const { query } = ctx;
+    const entity = await strapi.entityService.findOne("api::home.home", 1, {
+      ...query,
+    });
+    return getHome(entity);
+  },
+}));
